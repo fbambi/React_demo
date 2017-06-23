1b738efda92292bdd9f5447bab677e9e01e8206a
liudezhi <ldzwow@foxmail.com>
2017-06-24 00:56:23 +0800

    fix

--- main.js
+++ main.js
@@ -120,7 +120,6 @@ var Menu = React.createClass({
       //   console.log(element.name,element.markedUnselected?'marked':'unmarked');
       // });
       this.setState({
-<<<<<<< HEAD
         dishes:this.state.dishes
       });
     },
@@ -148,19 +147,12 @@ var Menu = React.createClass({
       this.props.submitOrders({
         sum:sum,
         finished:false
-=======
-        'dishes':this.state.dishes
->>>>>>> 5aee51f4ff43936bce0b8dde07cd508de685eed8
       });
     },
 
     render: function() {
       var that=this;
-<<<<<<< HEAD
 
-=======
-      console.log('render');
->>>>>>> 5aee51f4ff43936bce0b8dde07cd508de685eed8
 
         var unselectedDishesArr = this.state.dishes.map(function(dishes,index) {
           if(dishes.selected){
@@ -192,10 +184,7 @@ var Menu = React.createClass({
               <button onClick={this.addSelectedDishes}>add</button>
               <button onClick={this.removeSelectedDishes}>remove</button>
               <ul style={ulStyle}>{selectedDishesArr}</ul>
-<<<<<<< HEAD
               <button onClick={this.submitOrders}>submit</button>
-=======
->>>>>>> 5aee51f4ff43936bce0b8dde07cd508de685eed8
               </div>
               ;
 
@@ -207,7 +196,6 @@ var Menu = React.createClass({
 });
 
 
-<<<<<<< HEAD
 var Counter=React.createClass({
   getInitialState:function(){
     return {
@@ -243,8 +231,6 @@ var Counter=React.createClass({
 });
 
 
-=======
->>>>>>> 5aee51f4ff43936bce0b8dde07cd508de685eed8
 
 
 
@@ -252,7 +238,6 @@ var Counter=React.createClass({
 var Hall = React.createClass({
 
   getInitialState: function() {
-<<<<<<< HEAD
     return {
       orders:[]
     };
@@ -266,21 +251,13 @@ submitOrders:function(obj){
     orders:this.state.orders
   });
 },
-=======
-    return {hall: 1};
-  },
->>>>>>> 5aee51f4ff43936bce0b8dde07cd508de685eed8
 
   render: function() {
 
     return <div>
           <span>hall</span>
-<<<<<<< HEAD
           <Menu submitOrders={this.submitOrders.bind(this)} />
           <Counter orders={this.state.orders} />
-=======
-          <Menu />
->>>>>>> 5aee51f4ff43936bce0b8dde07cd508de685eed8
           </div>
     ;
   }
